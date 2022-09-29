15f3f8fe9ca0df93fcce6cb923b13c22af6df045
tmkoth431 <tobykothe@gmail.com>
2022-09-29 15:14:23 -0500

    location interface

--- src/Utils/LocationMap.java
+++ src/Utils/LocationMap.java
@@ -1,23 +1,25 @@
 package Utils;
 
 import Objects.Location;
+import Objects.Road;
+import Objects.Town;
 
 public class LocationMap {
-  private static final Location empty = new Location("empty", false, false, false, false, false);
+  private static final Location empty = new Road("empty", false, false, false, false);
 
-  private static final Location roadN = new Location("road", true, false, false, false, false);
-  private static final Location roadE = new Location("road", false, true, false, false, false);
-  private static final Location roadS = new Location("road", false, false, true, false, false);
-  private static final Location roadW = new Location("road", false, false, false, true, false);
+  private static final Location roadN = new Road("road", true, false, false, false);
+  private static final Location roadE = new Road("road", false, true, false, false);
+  private static final Location roadS = new Road("road", false, false, true, false);
+  private static final Location roadW = new Road("road", false, false, false, true);
 
-  private static final Location roadNE = new Location("road", true, true, false, false, false);
-  private static final Location roadNS = new Location("road", true, false, true, false, false);
-  private static final Location roadNW = new Location("road", true, false, false, true, false);
-  private static final Location roadES = new Location("road", false, true, true, false, false);
-  private static final Location roadEW = new Location("road", false, true, false, true, false);
-  private static final Location roadSW = new Location("road", false, false, true, true, false);
+  private static final Location roadNE = new Road("road", true, true, false, false);
+  private static final Location roadNS = new Road("road", true, false, true, false);
+  private static final Location roadNW = new Road("road", true, false, false, true);
+  private static final Location roadES = new Road("road", false, true, true, false);
+  private static final Location roadEW = new Road("road", false, true, false, true);
+  private static final Location roadSW = new Road("road", false, false, true, true);
 
-  private static final Location ftown = new Location("first town", true, false, true, false, true);
+  private static final Location ftown = new Town("first town", true, false, true, false);
 
   /**
    * @apiNote [Y,X] north is -y
